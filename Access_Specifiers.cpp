8dd22a9d9e0571f58ada2bbc2fec9c9d45c6abf9
Abuzaid-0132 <mohdabuzaid0132@gmail.com>
2024-06-15 01:06:24 +0530

    Acess specifiers in oop's

--- Access_Specifiers.cpp
+++ Access_Specifiers.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 class Teacher
 {
-public:
     // properties/ attributes
+private:
     double salary;
+
+public:
     string name;
     string dept;
     string subject;
@@ -14,6 +17,18 @@ public:
     void changeDept(string newDept)
     {
         dept = newDept;
+    }
+
+    // setter
+    void setSalary(double newSalary)
+    {
+        salary = newSalary;
+    }
+
+    // getter
+    double getSalary()
+    {
+        return salary;
     }
 };
 
@@ -24,7 +39,9 @@ int main()
     obj1.name = "Sara";
     obj1.subject = "C++";
     obj1.dept = "Computer Science";
+    obj1.setSalary(75000);
 
+    cout << obj1.getSalary() << endl;
     cout << obj1.dept << endl;
     obj1.changeDept("Information Technology");
     cout << obj1.dept << endl;
