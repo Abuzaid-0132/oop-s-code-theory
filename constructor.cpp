fa76987de8882d96177ad4c109576f1a328c3726
Abuzaid-0132 <mohdabuzaid0132@gmail.com>
2024-06-15 15:08:17 +0530

    understanding default copy constructor & custom copy constructor...

--- constructor.cpp
+++ constructor.cpp
@@ -9,10 +9,10 @@ private:
 
 public:
     // non-parametrized constructor
-    teacher()
-    {
-        dept = "Information Technology";
-    }
+    // teacher()
+    // {
+    //     dept = "Information Technology";
+    // }
 
     // parametrized constructor
     teacher(string nme, string dpt, string sub, double sal)
@@ -22,6 +22,16 @@ public:
         dept = dpt;
         subject = sub;
         salary = sal;
+    }
+
+    // copy constructor.
+    teacher(teacher &orgObj)
+    { // pass by reference.
+        cout << "I am custom copy constructor..." << endl;
+        this->name = orgObj.name;
+        this->dept = orgObj.dept;
+        this->subject = orgObj.subject;
+        this->salary = orgObj.salary;
     }
 
     string name;
@@ -45,7 +55,10 @@ public:
 int main()
 {
     teacher t1("Zain", "Computer Science", "DBMS", 75000); // constructor called...
-    t1.getInfo();
+    // t1.getInfo();
+
+    teacher t2(t1); // custom copy constructor invoked.
+    t2.getInfo();
 
     return 0;
 }
